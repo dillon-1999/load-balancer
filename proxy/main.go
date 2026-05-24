b8a10a1cc38f118216dea604605d613ff784b9a3
dillon-1999 <60618033+dillon-1999@users.noreply.github.com>
2026-05-24 11:36:27 -0500

    feat: remove the hard-coded backend list and convert to use the docker daemon sdk to discovery them

--- proxy/main.go
+++ proxy/main.go
@@ -1,44 +1,63 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os"
+	"strings"
 	"sync/atomic"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/events"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 )
 
+// used for string parsing
 type Backends struct {
 	Backends []string `json:"backends"`
 }
+
+type ContainerBackend struct {
+	ContainerID string
+	URL         *url.URL
+}
+
 type Balancer struct {
-	backends []*url.URL
-	next     atomic.Uint64
-	proxy    *httputil.ReverseProxy
+	backends []ContainerBackend
+
+	next  atomic.Uint64
+	proxy *httputil.ReverseProxy
 }
 
 func (b *Balancer) pick() *url.URL {
 	i := b.next.Add(1)
-	return b.backends[int(i)%len(b.backends)]
+	return b.backends[int(i)%len(b.backends)].URL
 }
 
 func (b *Balancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	b.proxy.ServeHTTP(w, r)
 }
-func NewBalancer(backends []string) *Balancer {
+
+func NewBalancer(backends []container.Summary) *Balancer {
 	b := &Balancer{}
 	for _, u := range backends {
-		u, err := url.Parse(u)
+
+		url, err := prepareUrlFromContainer(u)
 		if err != nil {
 			panic(err)
 		}
-		b.backends = append(b.backends, u)
+		backend := ContainerBackend{u.ID, url}
+		b.backends = append(b.backends, backend)
 	}
 
 	b.proxy = &httputil.ReverseProxy{
 		Rewrite: func(pr *httputil.ProxyRequest) {
 			target := b.pick()
+			fmt.Println(target)
 			pr.SetURL(target)
 			pr.Out.Host = target.Host
 		},
@@ -46,15 +65,71 @@ func NewBalancer(backends []string) *Balancer {
 	return b
 }
 
-func main() {
-	data, err := os.ReadFile("config.json")
+func getContainers(options container.ListOptions) []container.Summary {
+	apiClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
-	var backends Backends
-	if err := json.Unmarshal(data, &backends); err != nil {
+	defer apiClient.Close()
+
+	filterArgs := filters.NewArgs()
+	filterArgs.Add("label", "service=hello")
+	containers, err := apiClient.ContainerList(context.Background(), options)
+	if err != nil {
 		panic(err)
 	}
-	b := NewBalancer(backends.Backends)
-	http.ListenAndServe(":8000", b)
+	return containers
+}
+
+func listenForEvents(cli *client.Client, options events.ListOptions) {
+	msgChan, errChan := cli.Events(context.Background(), options)
+	for {
+		select {
+		case msg := <-msgChan:
+			fmt.Printf("Event: %s | Container: %s | Image: %s\n",
+				msg.Action, msg.Actor.ID[:12], msg.Actor.Attributes["image"])
+		case err := <-errChan:
+			if err != nil {
+				log.Fatalf("Event stream error: %v", err)
+			}
+			return
+		}
+	}
+}
+
+func prepareUrlFromContainer(c container.Summary) (*url.URL, error) {
+	if len(c.Names) == 0 {
+		return nil, fmt.Errorf("no name provided in container summary for container %s", c.ID)
+	}
+	name := strings.ReplaceAll(c.Names[0], "/", "")
+	if len(c.Ports) == 0 {
+		fmt.Errorf("no port open for container %s", c.ID)
+	}
+	port := c.Ports[0]
+	url_str := fmt.Sprintf("http://%s:%d", name, port.PrivatePort)
+	fmt.Println(url_str)
+	url, err := url.Parse(url_str)
+	if err != nil {
+		return nil, err
+	}
+	return url, err
+}
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	filterArgs := filters.NewArgs()
+	filterArgs.Add("label", "service=hello")
+
+	listOptions := container.ListOptions{Filters: filterArgs}
+
+	containers := getContainers(listOptions)
+
+	proxy := NewBalancer(containers)
+
+	http.ListenAndServe(":8000", proxy)
 }
